b9d0e3d351ef7ff49c78d37a2534384c99723a99
JosueToe <jtole043@fiu.edu>
2024-01-06 16:36:08 -0500

    Character Movement/TestMap
    This is the character movement rough draft and the test map rough draft to begin working on UI Still work to be made on this part before UI

---
+++ Unity Devlog Main/SwordAttack.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordAttack : MonoBehaviour
+{
+
+    public enum AttackDirection
+    {
+        left, right
+    }
+
+    public AttackDirection attackdirection;
+
+    Vector2 RightAttackOffset;
+    Collider2D swordCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        swordCollider = GetComponent<Collider2D>();
+        RightAttackOffset = transform.position;
+    }
+
+    public void Attack()
+    {
+        switch (attackdirection)
+        {
+            case AttackDirection.left:
+                AttackLeft();
+                break;
+            case AttackDirection.right:
+                AttackRight();
+                break;
+        }
+    }
+
+
+    private void AttackRight()
+    {
+        swordCollider.enabled = true;
+        transform.position = RightAttackOffset;
+    }
+
+    private void AttackLeft()
+    {
+        swordCollider.enabled = true;
+        transform.position = new Vector3(RightAttackOffset.x * -1, RightAttackOffset.y);
+    }
+
+    public void StopAttack()
+    {
+        swordCollider.enabled = false;
+    }
+
+}
